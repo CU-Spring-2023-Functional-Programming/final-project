d8c8c80989e1bab9ddec8e100926ebcbf743899b
nikojpapa <npapa1994@gmail.com>
2023-04-13 17:07:36 -0600

    setup test

--- final_project/main.ml
+++ final_project/main.ml
@@ -3,34 +3,49 @@ let asrt = function
  | (false, str) -> failwith ("Assertion failure: "^str)
 
 
-module TestCase = struct
-  let run run_state program = run_state program
-end
-
-
 module WasRun = struct
+  let setUp = fun (wasRun, _) -> ((wasRun, true), ())
+
+  let wasRun () = fun (wasRun, wasSetUp) -> ((wasRun, wasSetUp), wasRun)
+  let wasSetUp () = fun (wasRun, wasSetUp) -> ((wasRun, wasSetUp), wasSetUp)
+  let testMethod = fun (_, wasSetUp) -> ((true, wasSetUp), ())
+
+  let init = (false, false)
+  let run program =
+    let (state1, _) = setUp init in
+    let (state2, result) = program state1 in result
+
   let ( >>= ) m f = fun s ->
       let (s', v) = m s in
       f v s'
-
-  let wasRun (previousResult) = fun () -> ((), previousResult)
-  let testMethod = fun () -> ((), true)
-
-  let run_state i = let (s, v) = i () in v
 end
 
 
 module TestCaseTest = struct
-    let (let*) = WasRun.( >>= )
-    let testRunning () =
-        let program = WasRun.wasRun false in
-        let _ = asrt (not (TestCase.run WasRun.run_state program), "It should have begun with no runs") in
-        let program =
+    let setUp = fun (_) ->
+        let (let*) = WasRun.( >>= ) in
+        let program = begin
             let* testMethod = WasRun.testMethod in
-            WasRun.wasRun testMethod in
-        asrt (TestCase.run WasRun.run_state program, "It should have run at least once")
+            WasRun.wasRun testMethod
+        end in (program, ())
 
-    let run_state i = i ()
+    let testRunning = fun program ->
+        let _ = asrt (WasRun.run program, "It should have run") in
+        (program, ())
+
+    let testSetUp = fun program ->
+        let _ = asrt (WasRun.run program, "It should have been set up") in
+        (program, ())
+
+  let init = ()
+  let run program =
+    let (state1, _) = setUp init in
+    let (state2, result) = program state1 in result
+
+  let ( >>= ) m f = fun s ->
+      let (s', v) = m s in
+      f v s'
 end
 
-let _ = TestCase.run TestCaseTest.run_state TestCaseTest.testRunning
+let _ = TestCaseTest.run TestCaseTest.testRunning
+let _ = TestCaseTest.run TestCaseTest.testSetUp
